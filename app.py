f201b43d5adde6d8df984cc258818ed736adbb8c
Jatin Rajput <JTDAS333@outlook.com>
2026-04-17 21:15:11 +0530

    Change Flask app run configuration
    Updated the Flask app to run on host '0.0.0.0' and port 5000.

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def extract():
         return jsonify({"success": False, "error": "Target video not found in payload."})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
